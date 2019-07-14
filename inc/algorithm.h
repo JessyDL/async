9e3fd2152e3d27852776cc634f0c9fc43b1a2eeb
JessyDL <jessydelannoit@gmail.com>
2019-07-14 20:01:25 +0200

    added parallel execution draft

--- inc/algorithm.h
+++ inc/algorithm.h
@@ -15,6 +15,12 @@ namespace ASYNC_NAMESPACE
 	namespace details
 	{
 		template<typename T>
+		struct is_tuple : std::false_type {};
+
+
+		template<typename... Ts>
+		struct is_tuple<std::tuple<Ts...>> : std::true_type {};
+		template<typename T>
 		struct state_object
 		{
 			std::mutex mutex;
@@ -31,7 +37,6 @@ namespace ASYNC_NAMESPACE
 			void set(Args&& ... args)
 			{
 				auto lock = std::unique_lock(state->mutex);
-				//state->data.emplace<I>((args...);
 				state->data.template emplace<I>(std::forward<Args>(args)...);
 				state->condition_variable.notify_one();
 			}
@@ -43,7 +48,7 @@ namespace ASYNC_NAMESPACE
 		};
 
 		template<typename param_t, typename fn_t>
-		struct promise_like
+		struct promise_wrapper
 		{
 			param_t param;
 			fn_t func;
@@ -51,7 +56,14 @@ namespace ASYNC_NAMESPACE
 			template<typename... Args>
 			auto set_value(Args&& ... args)
 			{
-				param.set_value(std::invoke(func, std::forward<Args>(args)...));
+				if constexpr (is_tuple<param_t>::value)
+				{
+					param.set_value(std::apply(func, std::forward<Args>(args)...));
+				}
+				else
+				{
+					param.set_value(std::invoke(func, std::forward<Args>(args)...));
+				}
 			}
 
 			template<typename T>
@@ -75,71 +87,164 @@ namespace ASYNC_NAMESPACE
 				typedef typename std::tuple_element<i, std::tuple<Args...>>::type type;
 			};
 		};
+
+
+		template<typename T>
+		struct type_wrapper
+		{
+			using type = T;
+		};
+
+		template<typename T>
+		using promise_variant = std::variant<details::promise<T>, std::promise<T>>;
+
+		template<typename T>
+		struct is_variant : std::false_type {};
+
+
+		template<typename... Ts>
+		struct is_variant<std::variant<Ts...>> : std::true_type {};
+
+
 	}
+
+	struct invocation_index
+	{
+		operator size_t() const noexcept { return index; };
+		size_t index{};
+	};
 
 
 	template<typename FN1, typename FN2>
 	auto then(FN1&& task1, FN2&& task2)
 	{
-		auto lambda = [task1 = task1, task2 = task2](auto p)
+		return[task1 = std::forward<FN1>(task1), task2 = std::forward<FN2>(task2)](auto p)
 		{
 			using param_t = decltype(p);
-			using fn_t = decltype(task2);
 
-
-			if constexpr (std::is_same< param_t, void*>::value)
+			if constexpr (std::is_same<param_t, void*>::value)
 			{
-				return 0;
-				//return std::declval<typename details::function_traits<decltype(std::function(std::declval<FN2>()))>::result_type>();
+				return details::type_wrapper<typename details::function_traits<decltype(std::function{ std::declval<FN2>() }) > ::result_type > {};
 			}
 			else
 			{
-				std::invoke(task1, details::promise_like<param_t, fn_t>{std::forward<param_t>(p), task2});
+				if constexpr (details::is_variant<decltype(p)>::value)
+				{
+					std::visit([&](auto&& value) {
+						std::invoke(task1, details::promise_wrapper<decltype(value), decltype(task2)>{std::forward<decltype(value)>(value), task2}); }, p);
+				}
+				else
+				{
+					std::invoke(task1, details::promise_wrapper<param_t, decltype(task2)>{std::forward<param_t>(p), task2});
+				}
 			}
 		};
-
-		return lambda;
-
 	}
 
 	template<typename FN1, typename FN2, typename ... FNn>
 	auto then(FN1&& task1, FN2&& task2, FNn&& ... tasks)
 	{
-		return then(then(task1, task2), std::forward<FNn>(tasks)...);
+		return then(then(std::forward<FN1>(task1), std::forward<FN2>(task2)), std::forward<FNn>(tasks)...);
 	}
 
-	template<typename Task, typename T = decltype(std::declval<Task>()(std::declval<void*>()))>
-	T sync_wait(Task && task)
+
+	namespace details
 	{
-		details::state_object<T> state;
-
-		task(details::promise<T>{&state});
-
+		template<typename FN, typename... FNn>
+		auto into_impl(FN&& task2, FNn&& ... tasks)
 		{
-			auto lock = std::unique_lock{ state.mutex };
-			state.condition_variable.wait(lock, [&state]()
-				{return state.data.index() != 0; });
+			return then([tasks = std::forward_as_tuple(tasks...)](auto p)
+			{
+				std::apply([&](auto&&... args) 
+					{
+						p.set_value(compute<execution::wait>(args)...);
+					}, std::move(tasks));
+			}, std::forward<FN>(task2));
 		}
 
-		if (state.data.index() == 1)
-			std::rethrow_exception(std::get<1>(state.data));
+		template <typename... T, std::size_t... I>
+		auto subtuple_(std::tuple<T...>&& t, std::index_sequence<I...>) {
+			return std::make_tuple(std::get<I>(t)...);
+		}
 
-		return std::move(std::get<2>(state.data));
+		template <int Trim, typename... T>
+		auto subtuple(const std::tuple<T...>& t) {
+			return subtuple_(t, std::make_index_sequence<sizeof...(T) - Trim>());
+		}
 	}
 
-	template<typename Task, typename T = decltype(std::declval<Task>()(std::declval<void*>()))>
-	std::future<T> async_compute(Task && task)
+	template<typename... FNn>
+	auto into(FNn&&... tasks)
 	{
-		details::state_object<T> state;
-
-		std::promise<T> promise;
-		auto future = promise.get_future();
-		task(std::move(promise));
-		return future;
+		auto task2 = std::get<sizeof...(FNn) - 1 >(std::tie(tasks...));
+		auto subtuple = details::subtuple_(std::forward_as_tuple(tasks...), std::make_index_sequence<sizeof...(FNn) - 1>());
+		return then([tasks = std::forward<decltype(subtuple)>(subtuple)](auto p)
+		{
+			std::apply([&](auto&& ... args)
+				{
+					p.set_value(compute<execution::wait>(args)...);
+				}, std::move(tasks));
+		}, std::forward<decltype(task2 )>(task2));
 	}
 
+	template<typename FN>
+	auto parallel_n(FN&& task, size_t count = 0)
+	{
+		return [task = std::forward<FN>(task), count](auto p)
+		{
+			using result_t = typename decltype(std::declval<FN>()(std::declval<void*>()))::type;
 
-	template<execution ex = execution::async, typename Task, typename T = decltype(std::declval<Task>()(std::declval<void*>()))>
+			if constexpr (std::is_same<decltype(p), void*>::value)
+			{
+				return details::type_wrapper<std::vector<std::future<result_t>>>{};
+			}
+			else
+			{
+				std::vector<std::future<result_t>> futures;
+				for (auto i = 0u; i < count; ++i)
+				{
+					futures.emplace_back(std::move(compute<execution::async>(task)));
+				}
+				p.set_value(std::move(futures));
+			}
+		};
+	}
+
+	template<typename... FN>
+	auto parallel(FN&& ... tasks)
+	{
+		static_assert(sizeof...(FN) > 1);
+		return[tasks = std::forward_as_tuple(tasks...)](auto p)
+		{
+			using result_t = typename decltype(std::declval<decltype(std::get<0>(std::declval<std::tuple<FN...>>()))>()(std::declval<void*>()))::type;
+
+			if constexpr (std::is_same<decltype(p), void*>::value)
+			{
+				return details::type_wrapper<std::vector<std::future<result_t>>>{};
+			}
+			else
+			{
+				std::vector<std::future<result_t>> futures;
+
+				std::apply([&](auto&& ... task)
+					{
+						(futures.emplace_back(std::move(compute<execution::async>(task))), ...);
+					}, tasks);
+
+				if constexpr (details::is_variant<decltype(p)>::value)
+				{
+					std::visit([&](auto&& value) {
+						value.set_value(std::move(futures));}, p);
+				}
+				else
+				{
+					p.set_value(std::move(futures));
+				}
+			}
+		};
+	}
+
+	template<execution ex = execution::async, typename Task, typename T = typename decltype(std::declval<Task>()(std::declval<void*>()))::type>
 	auto compute(Task && task)
 	{
 		if constexpr (ex == execution::async)
@@ -155,7 +260,7 @@ namespace ASYNC_NAMESPACE
 		{
 			details::state_object<T> state;
 
-			task(details::promise<T>{&state});
+			task(details::promise_variant<T>{details::promise<T>{&state}});
 
 			{
 				auto lock = std::unique_lock{ state.mutex };
--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -3,15 +3,40 @@
 
 auto thread_like() { return [](auto p)
 {
-		p.set_value();
+	p.set_value();
 }; }
-auto new_thread() { return [](auto p) {std::thread t{ [p = std::move(p)] () mutable {p.set_value(); } }; t.detach(); }; }
+auto new_thread()
+{ 
+	return [](auto p) 
+	{
+		std::thread t
+		{ 
+			[p = std::move(p)] () mutable 
+			{
+				p.set_value(); 
+			} 
+		}; 
+		t.detach();
+	}; 
+}
 
+
+template<typename LT>
+class task_object
+{
+public:
+	task_object(LT&& lambda) : lambda(std::forward<LT>(lambda)) {};
+
+	template<typename... Args>
+	auto operator()(Args&& ... args) { return std::invoke(lambda, std::forward<Args>(args)...); }
+	LT lambda;
+};
 
 int main()
 {
+	auto thread_like_task = task_object{ thread_like() };
 	using namespace ASYNC_NAMESPACE;
-	auto task1 = then(thread_like(), []()
+	auto task = then(thread_like(), []()
 		{
 			return 42;
 		}, [](int value) -> int
@@ -24,10 +49,32 @@ int main()
 			return 42;
 		});
 
-	//auto task2 = then(task1, );
+
+	auto t1 = then(new_thread(), []() { return true; });
+	auto t2 = then(new_thread(), []() { return 10; });
+	auto t3 = into(t1, t2, [](bool truth, int value) { return (truth) ? 100 : 2; });
 
 
-	auto x = compute<execution::wait>(task1);
+	auto t4 = parallel_n(t1, 2);
+	auto t5 = parallel(then(new_thread(), []() { return 30; }), t2, t2);
+	auto t6 = into(t5, [](std::vector<std::future<int>> values) 
+		{ 
+			int sum = 0;
+			for (auto& future : values)
+			{
+				sum += future.get();
+			}
+			return sum;
+		});
+	//auto t3 = details::into_impl([](bool truth, int value) { return (truth) ? 100 : 2; }, t1, t2);
 
-	return 0;
+	//token >> token1 >> parallel(8, token2) >> print_token;
+
+	//auto tuple = std::tuple{ compute<execution::wait>(t1), compute<execution::wait>(t2)};
+
+	auto x = compute<execution::wait>(t3);
+
+	auto y = compute<execution::async>(t6);
+
+		return 0;
 }
